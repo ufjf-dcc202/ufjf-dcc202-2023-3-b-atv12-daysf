464d9b89cac0de2529f932a7a9a8667e02983a6b
Yan Dias Ferreira <13732727750@estudante.ufjf>
2023-11-28 23:05:57 -0300

    criei

---
+++ estoque.js
@@ -0,0 +1,16 @@
+let estoque = {
+    'joao': {
+        'maca': 1,
+        'banana': 2
+    },
+    'maria': {
+        'maca': 1,
+        'pera': 3
+    }
+};
+
+function getEstoque(){
+    return structuredClone(estoque);
+}
+
+export {getEstoque};
