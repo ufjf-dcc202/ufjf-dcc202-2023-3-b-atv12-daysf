6875d00f1c5dbc692a5bf8bb394dbde95d011dc3
Yan Dias Ferreira <13732727750@estudante.ufjf>
2023-11-28 22:31:47 -0300

    att ate o const destino

---
+++ main.js
@@ -0,0 +1,12 @@
+document.entrada.addEventListener('submit', leFormulario);
+
+function leFormulario(event) {
+    event.preventDefault();
+    const quantidade = document.entrada.quantidade.value;
+    const fruta = document.entrada.fruta.value;
+    const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
+
+console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
+// document.entrada.submit();
+}
